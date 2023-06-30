700d676f5d55313b9a3475c68828ec029dee6941
Mariel <mmmdagohoy2021@plm.edu.ph>
2023-06-30 22:48:34 +0800

    update
    - added file
    - update code

--- style.js
+++ style.js
@@ -1,9 +1,13 @@
-var img=$('div');
-$(document).ready(function(e) {
-    $(document).mousemove(function(e) {
-        var positionLeft = e.clientX - img.width()/2;
-        var positionTop = e.clientY - img.height()/2;
-        img.css({'position': 'fixed', 'left': positionLeft, 'top': positionTop});
-        mousePositionValueDiv.text(e.clientX+', '+e.clientY);
-    });
-});
+var a;
+function toggle(){
+
+    if(a==1){
+        document.getElementById("hide_section").style.display="inline";
+        return a=0;
+    }
+    else{
+        document.getElementById("hide_section").style.display="none";
+        return a=1;
+    }
+
+}
