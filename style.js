e3c5250179b25ae4e237a0894b3b83ba363fd056
Mariel <mmmdagohoy2021@plm.edu.ph>
2023-09-08 02:59:55 +0800

    update

--- style.js
+++ style.js
@@ -29,3 +29,19 @@ function showPanel(panelIndex,colorCode) {
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
 showPanel(0,'#526D82');
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {  
+    if (document.body.scrollTop > 20  || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    } else {
+    mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE     and Opera
+}
